e619e4de4efe64224d6363514a498aeac6c4dbaa
Anagas <115894733+anagas-s@users.noreply.github.com>
2023-09-23 10:22:43 +0530

    Add player switching feature

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 //Selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El =document.getElementById('current--0');
@@ -9,9 +11,12 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
 
+
 diceEl.classList.add('hidden');
 
+const scores = [0,0]
 let currentScore =0;
+let activePlayer =0;
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', () => {
@@ -22,8 +27,15 @@ btnRoll.addEventListener('click', () => {
 
     if(dice!==1){
         currentScore+=dice;
-        current0El.textContent =currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        
     }else {
-
+        //Switch Player
+        currentScore =0;
+        document.getElementById(`current--${activePlayer}`).textContent=0;
+        activePlayer = activePlayer === 0 ? 1:0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
+        
     }
 })
