5f4bbd734d1017993082ccff905c93f437f4c28b
Anagas <115894733+anagas-s@users.noreply.github.com>
2023-09-23 11:16:03 +0530

    Add reset game feature

--- script.js
+++ script.js
@@ -14,10 +14,29 @@ const btnHold = document.querySelector('.btn--hold')
 
 diceEl.classList.add('hidden');
 
-const scores = [0,0]
-let currentScore =0;
-let activePlayer =0;
-let playing =true;
+let scores;
+let currentScore;
+let activePlayer;
+let playing;
+
+const init = function(){
+    
+    scores = [0,0]
+    currentScore =0;
+    activePlayer =0;
+    playing =true;
+
+    score0El.textContent =0;
+    score1El.textContent =0;
+    current0El.textContent =0;
+    current1El.textContent =0;
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+    
+}
+init();
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', () => {
@@ -50,7 +69,7 @@ btnHold.addEventListener('click', () => {
     scores[activePlayer] = scores[activePlayer]+currentScore;
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
-    if(scores[activePlayer] >= 20){
+    if(scores[activePlayer] >= 100){
         playing =false;
         diceEl.classList.add('hidden');
         document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
@@ -67,3 +86,5 @@ btnHold.addEventListener('click', () => {
     }
     }
 })
+
+btnNew.addEventListener('click', init);
